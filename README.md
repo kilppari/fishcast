8bdbd3c6b224975db285515f1f8045d41df782a0
Pekka Mäkinen <makinpek@gmail.com>
2025-02-02 01:10:49 +0200

    Added optional sea level consideration into the algorithm

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Considered factors are:
 - Air pressure changes
 - Wind direction
 - Moon phases
+- Sea level changes (optional)
 
 ## Prerequisites
 - Python 3.x
@@ -23,7 +24,7 @@ Considered factors are:
 pip install requests ephem pytz
 ```
 ## Usage
-Basic usage to get forecast of Helsinki for next 24 hours
+Basic example to get forecast of Helsinki for next 24 hours
 ```bash
 python fishcast.py -l Helsinki -hr 24
 ```
@@ -33,6 +34,27 @@ python fishcast.py -l Helsinki -hr 24
 - `--hours`, `-hr`: Number of forecast hours (default: 48)
 - `--timezone`, `-tz`: Timezone (default: Europe/Helsinki)
 - `--visualize`, `-v`: Visualize the hourly forecast in ASCII chart (default: False)
+- `--sealevel`, `-sl`: Location for sealevel measurement (default: None). Do not use if not fishing in sea.
+
+```bash
+python fishcast.py --help
+usage: fishcast.py [-h] [--timezone TIMEZONE] [--location LOCATION] [--hours HOURS] [--visualize] [--sealevel SEALEVEL]
+
+Calculate fishing forecast based on weather and moon data.
+
+options:
+  -h, --help            show this help message and exit
+  --timezone TIMEZONE, -tz TIMEZONE
+                        Timezone (default: Europe/Helsinki)
+  --location LOCATION, -l LOCATION
+                        Location in Finland (default: Oulu)
+  --hours HOURS, -hr HOURS
+                        Forecast hours (default: 48)
+  --visualize, -v       Visualize the forecast (default: OFF)
+  --sealevel SEALEVEL, -sl SEALEVEL
+                        Location for sealevel measurement (default: OFF) Possible values: Pietarsaari, Kemi, Porvoo, Vaasa, Turku, Rauma, Raahe, Oulu, Mantyluoto,      
+                        Kaskinen, Helsinki, Hanko, Hamina, Degerby
+```	
 
 ## Output
 The script provides:
@@ -40,49 +62,50 @@ The script provides:
 - Hourly fishing index forecast (with ASCII chart if using --visualize or -v)
 - Top 5 best fishing hours for the specified period
 
+
 Example:
 ```bash
-python fishcast.py -l Oulu -hr 10 -v
+python fishcast.py -l Oulu -hr 10 -sl Oulu -v 
 
 Moon phases:
-------------------------------------------------------------------------------------
+-------------
 Previous full moon:      2025-01-14 00:26
-Next full moon:          2025-02-12 15:53
 Previous new moon:       2025-01-29 14:35
+Next full moon:          2025-02-12 15:53
 Next new moon:           2025-02-28 02:44
 
 Fishing forecast for Oulu for next 10 hours:
-------------------------------------------------------------------------------------
-2025-02-01 22:00 - Index:   0 - Pressure: 1023.3 hPa (+0.3), Wind:  31.0° (4.4 m/s)
-2025-02-01 23:00 - Index:   0 - Pressure: 1023.5 hPa (+0.2), Wind:  37.0° (5.3 m/s)
-2025-02-02 00:00 - Index:  48 - Pressure: 1024.1 hPa (+0.6), Wind:  39.0° (4.6 m/s)
-2025-02-02 01:00 - Index:  48 - Pressure: 1025.1 hPa (+1.0), Wind:  41.0° (4.0 m/s)
-2025-02-02 02:00 - Index:  24 - Pressure: 1025.5 hPa (+0.4), Wind:  41.0° (4.5 m/s)
-2025-02-02 03:00 - Index:  24 - Pressure: 1026.0 hPa (+0.5), Wind:  40.0° (4.3 m/s)
-2025-02-02 04:00 - Index:   0 - Pressure: 1026.2 hPa (+0.2), Wind:  37.0° (4.5 m/s)
-2025-02-02 05:00 - Index:   0 - Pressure: 1026.4 hPa (+0.2), Wind:  32.0° (5.0 m/s)
-2025-02-02 06:00 - Index:  24 - Pressure: 1026.8 hPa (+0.4), Wind:  38.0° (5.4 m/s)
+---------------------------------------------
+2025-02-02 02:00 - Index:  24 - Pressure: 1024.9 hPa (+0.4), Wind:  38.0° (4.5 m/s) Sealevel: 4.6 cm (+0.0)
+2025-02-02 03:00 - Index:  48 - Pressure: 1025.5 hPa (+0.6), Wind:  41.0° (4.5 m/s) Sealevel: 3.6 cm (-1.0)
+2025-02-02 04:00 - Index:   4 - Pressure: 1026.0 hPa (+0.5), Wind:  40.0° (4.3 m/s) Sealevel: -2.4 cm (-6.0)
+2025-02-02 05:00 - Index: -10 - Pressure: 1026.0 hPa (+0.0), Wind:  33.0° (5.0 m/s) Sealevel: -6.4 cm (-4.0)
+2025-02-02 06:00 - Index:   0 - Pressure: 1026.3 hPa (+0.3), Wind:  42.0° (5.1 m/s) Sealevel: -6.4 cm (+0.0)
+2025-02-02 07:00 - Index:  48 - Pressure: 1026.9 hPa (+0.6), Wind:  46.0° (5.2 m/s) Sealevel: -4.4 cm (+2.0)
+2025-02-02 08:00 - Index:  58 - Pressure: 1027.7 hPa (+0.8), Wind:  49.0° (5.3 m/s) Sealevel: -1.4 cm (+3.0)
+2025-02-02 09:00 - Index:  48 - Pressure: 1028.3 hPa (+0.6), Wind:  51.0° (6.1 m/s) Sealevel: -3.4 cm (-2.0)
+2025-02-02 10:00 - Index: -10 - Pressure: 1028.5 hPa (+0.2), Wind:  51.0° (5.9 m/s) Sealevel: -8.4 cm (-5.0)
 
-Date/Time        │ Fishing Index
-─────────────────┼────────────────────────────────────────────────────────────
-Sat Feb-01 22:00 │
-Sat Feb-01 23:00 │
-Sun Feb-02 00:00 │████████████████████████████
-Sun Feb-02 01:00 │████████████████████████████
-Sun Feb-02 02:00 │██████████████
-Sun Feb-02 03:00 │██████████████
-Sun Feb-02 04:00 │
+Date/Time        │Fishing Index
+─────────────────┼────────────────────────────────────────────────────────────────────────────────
+Sun Feb-02 02:00 │███████████████████
+Sun Feb-02 03:00 │██████████████████████████████████████
+Sun Feb-02 04:00 │███
 Sun Feb-02 05:00 │
-Sun Feb-02 06:00 │██████████████
-─────────────────┼────────────────────────────────────────────────────────────
-                 0           20          40          60          80          100
-                 ┴───────────┴───────────┴───────────┴───────────┴───────────┴
+Sun Feb-02 06:00 │
+Sun Feb-02 07:00 │██████████████████████████████████████
+Sun Feb-02 08:00 │██████████████████████████████████████████████
+Sun Feb-02 09:00 │██████████████████████████████████████
+Sun Feb-02 10:00 │
+─────────────────┼────────────────────────────────────────────────────────────────────────────────
+                 0               20              40              60              80              100
+                 ┴───────────────┴───────────────┴───────────────┴───────────────┴───────────────┴
 
 Top 5 best fishing hours in Oulu in next 10 hours:
-------------------------------------------------------------------------------------
-Sun Feb-02 00:00 - Index: 48.0 - Pressure: 1024.1 hPa (+0.6), Wind: 39.0° (4.6 m/s)
-Sun Feb-02 01:00 - Index: 48.0 - Pressure: 1025.1 hPa (+1.0), Wind: 41.0° (4.0 m/s)
-Sun Feb-02 02:00 - Index: 24.0 - Pressure: 1025.5 hPa (+0.4), Wind: 41.0° (4.5 m/s)
-Sun Feb-02 03:00 - Index: 24.0 - Pressure: 1026.0 hPa (+0.5), Wind: 40.0° (4.3 m/s)
-Sun Feb-02 06:00 - Index: 24.0 - Pressure: 1026.8 hPa (+0.4), Wind: 38.0° (5.4 m/s)
+---------------------------------------------------
+2025-02-02 02:00 - Index:  24 - Pressure: 1024.9 hPa (+0.4), Wind:  38.0° (4.5 m/s) Sealevel: 4.6 cm (+0.0)
+2025-02-02 03:00 - Index:  48 - Pressure: 1025.5 hPa (+0.6), Wind:  41.0° (4.5 m/s) Sealevel: 3.6 cm (-1.0)
+2025-02-02 07:00 - Index:  48 - Pressure: 1026.9 hPa (+0.6), Wind:  46.0° (5.2 m/s) Sealevel: -4.4 cm (+2.0)
+2025-02-02 08:00 - Index:  58 - Pressure: 1027.7 hPa (+0.8), Wind:  49.0° (5.3 m/s) Sealevel: -1.4 cm (+3.0)
+2025-02-02 09:00 - Index:  48 - Pressure: 1028.3 hPa (+0.6), Wind:  51.0° (6.1 m/s) Sealevel: -3.4 cm (-2.0)
 ```
--- fishcast.py
+++ fishcast.py
@@ -20,19 +20,35 @@ import ephem, pytz
 from dataclasses import dataclass
 import argparse
 
-TIMEZONE = "Europe/Helsinki"
-LOCATION = "Oulu"
-FORECAST_HOURS = 48 # FMI API provides forecast data between 2-3 days into future
+# Geoids of places supported by FMI open data API to get sea level information
+GEOIDS = {
+    "Pietarsaari": "-10000618",
+    "Kemi": "-10017238",
+    "Porvoo": "-100669",
+    "Vaasa": "632978",
+    "Turku": "633679",
+    "Rauma": "639734",
+    "Raahe": "640276",
+    "Oulu": "643492",
+    "Mantyluoto": "646666",  # Pori Mäntyluoto
+    "Kaskinen": "653760",
+    "Helsinki": "658225",
+    "Hanko": "659101",
+    "Hamina": "659169",
+    "Degerby": "660415"  # Föglö Degerby
+}
 
-#ARGPARSER = None
+#http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::sealevel::point::timevaluepair&starttime=2025-02-01T23:00:00Z&endtime=2025-02-02T09:00:00Z&geoid=643492
 
 @dataclass
 class ForecastData:
     time: datetime
     pressure: float
+    pressure_diff: float
     windspeed: float
     winddirection: float
-    pressure_diff: float
+    sealevel: float
+    sealevel_diff: float
     fishing_index: float
 
 def calculate_pressure_points(pressure_diff: float) -> int:
@@ -155,11 +171,47 @@ def calculate_moon_phase_dates(reference_time_utc: datetime) -> tuple[datetime, 
         localize(next_new)
     )
 
-def calculate_fishing_index(current_data: ForecastData, prev_data: ForecastData = None):
+def calculate_sealevel_points(sealevel_diff: float) -> int:
+    """
+    Calculate points based on sea level change between consecutive measurements.
+    
+    Args:
+        sealevel_diff: float, difference in sea level (cm) between current and previous measurement
+        
+    Returns:
+        int: Points awarded based on sea level change:
+            - +30 points: > 9 cm rise
+            - +20 points: 6-9 cm rise
+            - +10 points: 3-6 cm rise
+            - -30 points: < -9 cm fall
+            - -20 points: -6 to -9 cm fall
+            - -10 points: -3 to -6 cm fall
+            - 0 points: changes between -3 and +3 cm
+            
+    Note:
+        Positive sea level changes (rising water) contribute positively to the fishing index,
+        while negative changes (falling water) contribute negatively.
+    """
+    if sealevel_diff >= 3 and sealevel_diff <= 6:
+        return 10
+    elif sealevel_diff > 6 and sealevel_diff <= 9:
+        return 20
+    elif sealevel_diff > 9:
+        return 30
+    elif sealevel_diff > -6 and sealevel_diff <= -3:
+        return -10
+    elif sealevel_diff >= -9 and sealevel_diff <= -6:
+        return -20
+    elif sealevel_diff < -9:
+        return -30
+    return 0
+
+def calculate_fishing_index(current_data: ForecastData, prev_data: ForecastData):
     """
     Calculate overall fishing conditions index based on weather and moon data.
     
     Args:
+
         current_data: ForecastData object containing current weather measurements
         prev_data: Optional ForecastData object containing previous measurements for
                    pressure difference calculation
@@ -167,14 +219,17 @@ def calculate_fishing_index(current_data: ForecastData, prev_data: ForecastData 
     COEFF_PRESSURE_CHANGE = 0.6
     COEFF_WIND_DIRECTION = 0.3
     COEFF_MOON_PHASE = 0.15
-    points = {'pressure': 0, 'wind': 0, 'moon': 0}
+    points = {'pressure': 0, 'wind': 0, 'moon': 0, 'sealevel': 0}
     
     # Calculate pressure points
-    if prev_data:
-        pressure_diff = current_data.pressure - prev_data.pressure
-        points['pressure'] = calculate_pressure_points(pressure_diff) * COEFF_PRESSURE_CHANGE
-
+    pressure_diff = current_data.pressure - prev_data.pressure
+    points['pressure'] = calculate_pressure_points(pressure_diff) * COEFF_PRESSURE_CHANGE
     current_data.pressure_diff = pressure_diff # Store the pressure difference
+
+    if ARGS.sealevel != None:
+        sealevel_diff = current_data.sealevel - prev_data.sealevel
+        points['sealevel'] = calculate_sealevel_points(sealevel_diff)
+        current_data.sealevel_diff = sealevel_diff # Store the sea level difference
     
     # Calculate wind direction points
     points['wind'] = calculate_wind_direction_points(current_data.winddirection) * COEFF_WIND_DIRECTION
@@ -204,9 +259,13 @@ def get_forecast(
         Each dictionary contains:
             - time: datetime, timestamp in specified timezone
             - pressure: float, air pressure in hPa
+            - pressure_diff: float, difference in pressure between current and previous measurement
             - windspeed: float, wind speed in m/s
             - winddirection: float, wind direction in degrees (0-360)
-    
+            - sealevel: float, sea level in cm
+            - sealevel_diff: float, difference in sea level between current and previous measurement
+            - fishing_index: float, fishing index
+
     Note:
         The FMI API provides data for Finnish locations only. The data is typically
         available in 1-hour intervals for the next 48 hours.
@@ -217,19 +276,20 @@ def get_forecast(
 
     end_time = start_time + timedelta(hours=hours)
     
-    url = (f"http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0"
-           f"&request=getFeature&storedquery_id=fmi::forecast::harmonie::surface::point::timevaluepair"
-           f"&place={place}&parameters=WindDirection,WindSpeedMS,Pressure"
-           f"&starttime={start_time.strftime('%Y-%m-%dT%H:%M:%S')}Z"
-           f"&endtime={end_time.strftime('%Y-%m-%dT%H:%M:%S')}Z")
+    # URL to get forecast data for surface measurements
+    url_surface = (f"http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0"
+        f"&request=getFeature&storedquery_id=fmi::forecast::harmonie::surface::point::timevaluepair"
+        f"&place={place}&parameters=WindDirection,WindSpeedMS,Pressure"
+        f"&starttime={start_time.strftime('%Y-%m-%dT%H:%M:%S')}Z"
+        f"&endtime={end_time.strftime('%Y-%m-%dT%H:%M:%S')}Z")
     
     try:
-        response = requests.get(url)
-        response.raise_for_status()
-        
+        response_surface = requests.get(url_surface)
+        response_surface.raise_for_status()
+
         # Start parsing XML response
-        root = ET.fromstring(response.content)
-        
+        root_surface = ET.fromstring(response_surface.content)
+
         # Find all wml2:point elements under the specific MeasurementTimeseries
         namespace = {'wml2': 'http://www.opengis.net/waterml/2.0', 'gml': 'http://www.opengis.net/gml/3.2'}
         
@@ -237,38 +297,55 @@ def get_forecast(
             timeseries = root.find(f'.//wml2:MeasurementTimeseries[@gml:id="mts-1-1-{parameter}"]', namespace)
             return timeseries.findall('.//wml2:point', namespace) if timeseries else []
         
-        points_pressure = get_measurement_points(root, 'Pressure')
-        points_windspeed = get_measurement_points(root, 'WindSpeedMS')
-        points_winddirection = get_measurement_points(root, 'WindDirection')
+        points_pressure = get_measurement_points(root_surface, 'Pressure')
+        points_windspeed = get_measurement_points(root_surface, 'WindSpeedMS')
+        points_winddirection = get_measurement_points(root_surface, 'WindDirection')
+
+        assert len(points_pressure) == len(points_windspeed) == len(points_winddirection)
 
         # Process pressure data and initialize forecast entries
         forecast_data = [
             {
-                'time': pytz.utc.localize(datetime.strptime(point.find('.//wml2:time', namespace).text, 
+                'time': pytz.utc.localize(datetime.strptime(point_pressure.find('.//wml2:time', namespace).text, 
                                                           '%Y-%m-%dT%H:%M:%SZ')).astimezone(pytz.timezone(timezone)),
-                'pressure': float(point.find('.//wml2:value', namespace).text),
-                'windspeed': 0.0,
-                'winddirection': 0.0,
-                'pressure_diff': 0.0,
-                'fishing_index': 0.0
+                'pressure': float(point_pressure.find('.//wml2:value', namespace).text),
+                'pressure_diff': 0.0, # To be calculated later  
+                'windspeed': float(point_windspeed.find('.//wml2:value', namespace).text),
+                'winddirection': float(point_winddirection.find('.//wml2:value', namespace).text),
+                'sealevel': 0.0, # To be calculated later
+                'sealevel_diff': 0.0, # To be calculated later
+                'fishing_index': 0.0, # To be calculated later
             }
-            for point in points_pressure
+            for point_pressure, point_windspeed, point_winddirection in
+              zip(points_pressure, points_windspeed, points_winddirection)
         ]
 
-        # Process wind speed data
-        for i, point in enumerate(points_windspeed):
-            forecast_data[i]['windspeed'] = float(point.find('.//wml2:value', namespace).text)
+        # Get sealevel data if requested
+        if ARGS.sealevel != None:
+            if ARGS.sealevel not in GEOIDS:
+                raise ValueError(f"Invalid sea level measurement location: {ARGS.sealevel}.\nPossible values: {', '.join(GEOIDS.keys())}")
 
-        # Process wind direction data
-        for i, point in enumerate(points_winddirection):
-            forecast_data[i]['winddirection'] = float(point.find('.//wml2:value', namespace).text)
+            url_sealevel = (f"http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature"
+                f"&storedquery_id=fmi::forecast::sealevel::point::timevaluepair&"
+                f"starttime={start_time.strftime('%Y-%m-%dT%H:%M:%S')}Z"
+                f"&endtime={end_time.strftime('%Y-%m-%dT%H:%M:%S')}Z&geoid={GEOIDS[ARGS.sealevel]}")
+            response_sealevel = requests.get(url_sealevel)
+            response_sealevel.raise_for_status()
+            root_sealevel = ET.fromstring(response_sealevel.content)
+            points_sealevel = get_measurement_points(root_sealevel, 'SeaLevelN2000')
+            assert len(points_pressure) == len(points_sealevel)
+
+            for i, point_sealevel in enumerate(points_sealevel):
+                forecast_data[i]['sealevel'] = float(point_sealevel.find('.//wml2:value', namespace).text)
 
         return forecast_data
         
     except requests.RequestException as e:
         print(f"Error fetching forecast data: {e}")
         return None
-
+    except Exception as e:
+        print(f"Error fetching forecast data: {e}")
+        return None
 
 def print_ascii_chart(fishing_index_forecast):
     """
@@ -281,13 +358,13 @@ def print_ascii_chart(fishing_index_forecast):
     if not fishing_index_forecast:
         return
 
-    MAX_WIDTH = 60  # Maximum width for the bars
+    MAX_WIDTH = 80  # Maximum width for the bars
     
     # Get max value for scaling
     max_index = 100
 
     print("")
-    print("Date/Time        │ Fishing Index")
+    print("Date/Time        │Fishing Index")
     print("─────────────────┼" + "─" * MAX_WIDTH)
     
     # Draw each hour's bar
@@ -303,12 +380,11 @@ def print_ascii_chart(fishing_index_forecast):
     print("─────────────────┼" + "─" * MAX_WIDTH)
     
     # Create scale with marks at 0%, 20%, 40%, 60%, 80% and 100% of max width
-    scale = "0".ljust(12)
-    scale += str(int(max_index * 0.2)).ljust(12)
-    scale += str(int(max_index * 0.4)).ljust(12)
-
-    scale += str(int(max_index * 0.6)).ljust(12)
-    scale += str(int(max_index * 0.8)).ljust(12)
+    scale = "0".ljust(int(MAX_WIDTH/5))
+    scale += str(int(max_index * 0.2)).ljust(int(MAX_WIDTH/5))
+    scale += str(int(max_index * 0.4)).ljust(int(MAX_WIDTH/5))
+    scale += str(int(max_index * 0.6)).ljust(int(MAX_WIDTH/5))
+    scale += str(int(max_index * 0.8)).ljust(int(MAX_WIDTH/5))
     scale += str(int(max_index))
     print(" " * 17 + scale)
 
@@ -322,9 +398,18 @@ def print_ascii_chart(fishing_index_forecast):
             ticks += "─"
     print(" " * 17 + ticks)
 
+def forecastdata_to_str(data: ForecastData) -> str:
+    str_sealevel = f"{data.sealevel:.1f} cm ({data.sealevel_diff:+.1f})" if ARGS.sealevel != None else "N/A"
+    
+    return (f"{data.time.strftime('%Y-%m-%d %H:%M')} - "
+            f"Index: {int(data.fishing_index):>3} - "
+            f"Pressure: {data.pressure:6.1f} hPa ({data.pressure_diff:+.1f}), "
+            f"Wind: {data.winddirection:5.1f}° ({data.windspeed:.1f} m/s) "
+            f"Sealevel: {str_sealevel}")
+
 # Run script
 if __name__ == "__main__":
-    global ARGPARSER
+
     parser = argparse.ArgumentParser(description='Calculate fishing forecast based on weather and moon data.')
     parser.add_argument('--timezone', '-tz', 
                        default="Europe/Helsinki",
@@ -338,7 +423,10 @@ if __name__ == "__main__":
                        help='Forecast hours (default: 48)')
     parser.add_argument('--visualize', '-v',
                        action='store_true',
-                       help='Visualize the forecast (default: False)')
+                       help='Visualize the forecast (default: OFF)')
+    parser.add_argument('--sealevel', '-sl',
+                        default=None,
+                        help=f'Location for sealevel measurement (default: OFF)\nPossible values: {", ".join(GEOIDS.keys())}')
 
     global ARGS
     ARGS = parser.parse_args()
@@ -348,18 +436,22 @@ if __name__ == "__main__":
                                     hours=ARGS.hours)
     fishing_index_forecast = []
     if forecast_data_list:
-        print("\nMoon phases:")
-        print("-" * 84)
+        text = "\nMoon phases:"
+        print(text)
+        print("-" * len(text))
         # Print out dates of past and future moon phases (Full moon and new moon)
         moon_phases = calculate_moon_phase_dates(datetime.now(timezone.utc))
+
         print(f"Previous full moon:\t {moon_phases[0].strftime('%Y-%m-%d %H:%M')}\n"
-              f"Next full moon:\t\t {moon_phases[1].strftime('%Y-%m-%d %H:%M')}\n"
               f"Previous new moon:\t {moon_phases[2].strftime('%Y-%m-%d %H:%M')}\n"
+              f"Next full moon:\t\t {moon_phases[1].strftime('%Y-%m-%d %H:%M')}\n"
               f"Next new moon:\t\t {moon_phases[3].strftime('%Y-%m-%d %H:%M')}")
     
-        print(f"\nFishing forecast for {ARGS.location} for next {ARGS.hours} hours:")
-        print("-" * 84)
+        text = f"\nFishing forecast for {ARGS.location} for next {ARGS.hours} hours:" 
+        print(text)
+        print("-" * len(text))
         for i, data in enumerate(forecast_data_list):
+
             if i == 0:
                 # Skip the first data point as it has no previous data for comparison.
                 continue
@@ -372,18 +464,16 @@ if __name__ == "__main__":
             # Store current data into list
             fishing_index_forecast.append(curr_data)
 
-            print(f"{curr_data.time.strftime('%Y-%m-%d %H:%M')} - "
-                f"Index: {int(curr_data.fishing_index):>3} - "
-                f"Pressure: {curr_data.pressure:6.1f} hPa ({curr_data.pressure_diff:+.1f}), "
-                f"Wind: {curr_data.winddirection:5.1f}° ({curr_data.windspeed:.1f} m/s)")
+            print(forecastdata_to_str(curr_data))
 
     if fishing_index_forecast:
         if ARGS.visualize:
             print_ascii_chart(fishing_index_forecast)
 
         # Print top 5 best fishing times
-        print(f"\nTop 5 best fishing hours in {ARGS.location} in next {ARGS.hours} hours:")
-        print("-" * 84)
+        text = f"\nTop 5 best fishing hours in {ARGS.location} in next {ARGS.hours} hours:"
+        print(text)
+        print("-" * len(text))
         
         # Get top 5 by fishing index, then sort by time
         best_times = sorted(
@@ -396,12 +486,8 @@ if __name__ == "__main__":
         )
         
         for data in best_times:
-            print(
-                f"{data.time.strftime('%a %b-%d %H:%M')} - "
-                f"Index: {data.fishing_index:.1f} - "
-                f"Pressure: {data.pressure:.1f} hPa ({data.pressure_diff:+.1f}), "
-                f"Wind: {data.winddirection:.1f}° ({data.windspeed:.1f} m/s)"
-            )
+            print(forecastdata_to_str(data))
         print("")
+
 
 
